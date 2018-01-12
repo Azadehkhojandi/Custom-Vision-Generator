a2b37818f2a9fb8c3fdf8ddd11edceadb72394e2
Azadeh Khojandi <azadehkhojandi@gmail.com>
2018-01-12 13:58:41 +1100

    clean up

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -21,14 +22,30 @@ namespace CustomVisionEnd2End
         {
             try
             {
+                var stopWatch = new Stopwatch();
+
+                stopWatch.Start();
+
                 MainAsync(args).GetAwaiter().GetResult();
+
+                stopWatch.Stop();
+                // Get the elapsed time as a TimeSpan value.
+                var ts = stopWatch.Elapsed;
+
+                // Format and display the TimeSpan value.
+                var elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+                Console.WriteLine("RunTime " + elapsedTime);
+
+
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                Console.WriteLine($"\tPress any key to exit");
-                Console.ReadKey();
+
             }
+            Console.WriteLine($"\tPress any key to exit");
+            Console.ReadKey();
 
         }
 
@@ -49,29 +66,46 @@ namespace CustomVisionEnd2End
             Console.WriteLine($"\tReading Tags from {tsgfilepath}");
             var tags = ReadTags(tsgfilepath);
 
-            
+
             //download images - 
             var trainingSetPath = $"{basepath}\\{projectName}\\data\\TrainingSet";
             var testSetPath = $"{basepath}\\{projectName}\\data\\TestSet";
             Console.WriteLine($"\tBing search & downloading images - split them in TrainingSet & TestSet for each tag");
             Console.WriteLine($"\tTrainingSetPath: {trainingSetPath}");
             Console.WriteLine($"\tTestSetPath: {testSetPath}");
+            System.IO.Directory.CreateDirectory(trainingSetPath);
+            System.IO.Directory.CreateDirectory(testSetPath);
 
-            using (var bingImageSearchService = new BingImageSearchService())
+
+            int minTrainingPhotosCount = 5;
+            int minTestPhotosCount = 3;
+
+            foreach (var tag in tags)
             {
-                foreach (var tag in tags)
+                //var t = new Thread(async () =>
                 {
-                    Console.WriteLine($"\tStart Process for : {tag}");
-                    var bingresult = await bingImageSearchService.ImageSearch(tag, 20);
-                    if (bingresult.value != null)
+                    using (var bingImageSearchService = new BingImageSearchService())
                     {
+                        Console.WriteLine($"\tStart Process for : {tag}");
+
+                        var bingresult = await bingImageSearchService.ImageSearch(tag, 20);
+                        if (bingresult.value == null) return;
                         //training 
-                        DownloadImages($"{trainingSetPath}\\{tag}", bingresult.value.Take(10).ToList(),5);
+                        var trainingphotos = DownloadImages($"{trainingSetPath}\\{tag}", bingresult.value.Take(10).ToList(), minTrainingPhotosCount);
                         //test
-                        DownloadImages($"{testSetPath}\\{tag}", bingresult.value.Skip(10).ToList(),3);
+                        var testphotos = DownloadImages($"{testSetPath}\\{tag}", bingresult.value.Skip(10).ToList(), minTestPhotosCount);
+
+                        if (trainingphotos < minTrainingPhotosCount || testphotos < minTestPhotosCount)
+                        {
+                            throw new Exception($"Bing couldn't find required images.you need at least 2 tags and 5 images for each tag to start");
+                        }
                     }
                 }
+                //);
+
+
             }
+
 
 
             CreateTheModel(trainingSetPath, project);
@@ -83,8 +117,7 @@ namespace CustomVisionEnd2End
 
             TestingTheModel(testSetPath, project);
 
-            Console.WriteLine($"\tPress any key to exit");
-            Console.ReadKey();
+
         }
 
         private static List<string> ReadTags(string tsgfilepath)
@@ -111,15 +144,15 @@ namespace CustomVisionEnd2End
 
         }
 
-        private static void DownloadImages(string pathString, List<Value> items, int numberofimagestodownload = -1)
+        private static int DownloadImages(string pathString, List<Value> items, int numberofimagestodownload = -1)
         {
             var counter = 0;
             Directory.CreateDirectory(pathString);
             foreach (var item in items)
             {
-                if (numberofimagestodownload > 0 && counter > numberofimagestodownload)
+                if (numberofimagestodownload > 0 && counter >= numberofimagestodownload)
                 {
-                    return; 
+                    return numberofimagestodownload;
                 }
                 try
                 {
@@ -133,13 +166,16 @@ namespace CustomVisionEnd2End
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    //kill exception and carry on
+                    Console.WriteLine("\tImage download error: " + e.Message);
                 }
             }
+            return counter;
         }
 
         private static void TestingTheModel(string testingSetPath, Project project)
         {
+
             var predictionKey = ConfigurationManager.AppSettings["CustomVision_PredictionKey"];
             var predictionEndpoint = new PredictionEndpoint() { ApiKey = predictionKey };
 
@@ -158,161 +194,217 @@ namespace CustomVisionEnd2End
                 }).ToList();
                 foreach (var testImage in images)
                 {
-                    var dir = new DirectoryInfo(subdirectory);
-                    var label = dir.Name;
-                    labels.Add(label);
-
-                    Console.WriteLine($"\tActucal tag: {label}");
-
-                    var result = predictionEndpoint.PredictImage(project.Id, testImage);
-                    var predictedClass = result.Predictions[0].Tag;
-                    var predictedProb = result.Predictions[0].Probability;
-
-                    var key = $"{label}|{predictedClass}";
-                    if (!predictionResult.ContainsKey(key))
+                    try
                     {
-                        predictionResult.Add(key, 0);
-                    }
-                    predictionResult[key] = predictionResult[key] + 1;
+                        var dir = new DirectoryInfo(subdirectory);
+                        var label = dir.Name;
+                        labels.Add(label);
 
-                    // Loop over each prediction and write out the results
-                    foreach (var c in result.Predictions)
-                    {
-                        Console.WriteLine($"\t{c.Tag}: {c.Probability:P1}");
+                        Console.WriteLine($"\tActucal tag: {label}");
+
+                        var result = predictionEndpoint.PredictImage(project.Id, testImage);
+                        var predictedClass = result.Predictions[0].Tag;
+                        var predictedProb = result.Predictions[0].Probability;
+
+                        var key = $"{label}|{predictedClass}";
+                        if (!predictionResult.ContainsKey(key))
+                        {
+                            predictionResult.Add(key, 0);
+                        }
+                        predictionResult[key] = predictionResult[key] + 1;
+
+                        // Loop over each prediction and write out the results
+                        foreach (var c in result.Predictions)
+                        {
+                            Console.WriteLine($"\t{c.Tag}: {c.Probability:P1}");
+                        }
                     }
+                    catch (Exception e)
+                    {
+                        //kill exception and carry on
+                        Console.WriteLine(e);
+                    }
+
+
                 }
             }
-
-            using (TextWriter writer = new StreamWriter($"{testingSetPath}\\testModel.csv"))
+            try
             {
-                var csv = new CsvWriter(writer);
-
-                csv.WriteRecords(testModel);
-            }
-
-
-
-
-
-
-            labels = labels.Distinct().ToList();
-
-            // Two-dimensional array. [row,col]
-            var array2D = new string[labels.Count + 1, labels.Count + 1];
-
-            var colindex = 0;
-            var rowindex = 0;
-            foreach (var lable in labels)
-            {
-                array2D[rowindex, colindex] = lable;
-                rowindex = rowindex + 1;
-            }
-
-            colindex = 1;
-            rowindex = labels.Count;
-            foreach (var label in labels)
-            {
-                array2D[rowindex, colindex] = label;
-                colindex = colindex + 1;
-            }
-
-
-            rowindex = 0;
-
-            foreach (var trueLabel in labels)
-            {
-                colindex = 1;
-                foreach (var predictedLabel in labels)
+                using (TextWriter writer = new StreamWriter($"{testingSetPath}\\testModel.csv"))
                 {
-                    var key = $"{trueLabel}|{predictedLabel}";
-                    var value = 0;
-                    if (predictionResult.ContainsKey(key))
-                    {
-                        value = predictionResult[key];
+                    var csv = new CsvWriter(writer);
 
-                    }
-                    array2D[rowindex, colindex] = value.ToString();
-                    colindex = colindex + 1;
+                    csv.WriteRecords(testModel);
                 }
-                rowindex = rowindex + 1;
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+
+
+
+
+
+
+
+            var array2D = GenerateConfusionMatrix(labels, predictionResult);
 
             //pretty print
-
             PrettyPrint(array2D);
 
 
             ExporttoCSV(testingSetPath, array2D);
         }
 
+        private static string[,] GenerateConfusionMatrix(List<string> labels, Dictionary<string, int> predictionResult)
+        {
+            labels = labels.Distinct().ToList();
+
+            // Two-dimensional array. [row,col]
+            var array2D = new string[labels.Count + 1, labels.Count + 1];
+
+            try
+            {
+
+
+                var colindex = 0;
+                var rowindex = 0;
+                foreach (var lable in labels)
+                {
+                    array2D[rowindex, colindex] = lable;
+                    rowindex = rowindex + 1;
+                }
+
+                colindex = 1;
+                rowindex = labels.Count;
+                foreach (var label in labels)
+                {
+                    array2D[rowindex, colindex] = label;
+                    colindex = colindex + 1;
+                }
+
+
+                rowindex = 0;
+
+                foreach (var trueLabel in labels)
+                {
+                    colindex = 1;
+                    foreach (var predictedLabel in labels)
+                    {
+                        var key = $"{trueLabel}|{predictedLabel}";
+                        var value = 0;
+                        if (predictionResult.ContainsKey(key))
+                        {
+                            value = predictionResult[key];
+                        }
+                        array2D[rowindex, colindex] = value.ToString();
+                        colindex = colindex + 1;
+                    }
+                    rowindex = rowindex + 1;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+            return array2D;
+        }
+
         private static void ExporttoCSV(string testingSetPath, string[,] array2D)
         {
-            var rowLength = array2D.GetLength(1);
-            var colLength = array2D.GetLength(0);
-            //csv file
-            using (var file =
-                new StreamWriter($"{testingSetPath}\\result.csv"))
+            try
             {
-                for (var i = 0; i < rowLength; i++)
+                var rowLength = array2D.GetLength(1);
+                var colLength = array2D.GetLength(0);
+                //csv file
+                using (var file =
+                    new StreamWriter($"{testingSetPath}\\result.csv"))
                 {
-                    var item = new List<string>();
-                    for (var j = 0; j < colLength; j++)
+                    for (var i = 0; i < rowLength; i++)
                     {
-                        item.Add(array2D[i, j]);
+                        var item = new List<string>();
+                        for (var j = 0; j < colLength; j++)
+                        {
+                            item.Add(array2D[i, j]);
+                        }
+                        file.WriteLine(string.Join(",", item));
                     }
-                    file.WriteLine(string.Join(",", item));
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
 
         private static void PrettyPrint(string[,] array2D)
         {
-            var rowLength = array2D.GetLength(1);
-            var colLength = array2D.GetLength(0);
-
-            Console.WriteLine("-------------------------------");
-
-
-            for (var i = 0; i < rowLength; i++)
+            try
             {
-                for (var j = 0; j < colLength; j++)
+                var rowLength = array2D.GetLength(1);
+                var colLength = array2D.GetLength(0);
+
+                Console.WriteLine("-------------------------------");
+
+
+                for (var i = 0; i < rowLength; i++)
                 {
-                    Console.Write($"{array2D[i, j],-30}");
+                    for (var j = 0; j < colLength; j++)
+                    {
+                        Console.Write($"{array2D[i, j],-30}");
+                    }
+                    Console.Write(Environment.NewLine + Environment.NewLine);
                 }
-                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
             }
         }
 
         private static void TrainTheModel(Project project)
         {
-            var trainingKey = ConfigurationManager.AppSettings["CustomVision_TrainingKey"];
-            var trainingApi = new TrainingApi() { ApiKey = trainingKey };
-
-            var iteration = trainingApi.TrainProject(project.Id);
-
-            Console.WriteLine($"\tWaiting for training process finishes");
-            // The returned iteration will be in progress, and can be queried periodically to see when it has completed
-            while (iteration.Status == "Training")
+            try
             {
-                Console.WriteLine($"\t...");
+                var trainingKey = ConfigurationManager.AppSettings["CustomVision_TrainingKey"];
+                var trainingApi = new TrainingApi() { ApiKey = trainingKey };
 
-                Thread.Sleep(1000);
+                var iteration = trainingApi.TrainProject(project.Id);
 
-                // Re-query the iteration to get it's updated status
-                iteration = trainingApi.GetIteration(project.Id, iteration.Id);
+                Console.WriteLine($"\tWaiting for training process finishes");
+                // The returned iteration will be in progress, and can be queried periodically to see when it has completed
+                while (iteration.Status == "Training")
+                {
+                    Console.WriteLine($"\t...");
+
+                    Thread.Sleep(1000);
+
+                    // Re-query the iteration to get it's updated status
+                    iteration = trainingApi.GetIteration(project.Id, iteration.Id);
+                }
+
+                Console.WriteLine($"\tUpdating default iteration");
+                // The iteration is now trained. Make it the default project endpoint
+                iteration.IsDefault = true;
+                trainingApi.UpdateIteration(project.Id, iteration.Id, iteration);
             }
-
-            Console.WriteLine($"\tUpdating default iteration");
-            // The iteration is now trained. Make it the default project endpoint
-            iteration.IsDefault = true;
-            trainingApi.UpdateIteration(project.Id, iteration.Id, iteration);
-
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
 
 
         }
 
         private static void CreateTheModel(string trainingSetPath, Project project)
         {
+
             var trainingKey = ConfigurationManager.AppSettings["CustomVision_TrainingKey"];
             var trainingApi = new TrainingApi() { ApiKey = trainingKey };
 
@@ -335,28 +427,57 @@ namespace CustomVisionEnd2End
 
                 foreach (var image in images)
                 {
-                    Console.WriteLine($"\tUploading image with tag: {tag.Name}");
-                    trainingApi.CreateImagesFromData(project.Id, image, new List<string>() { tag.Id.ToString() });
+                    try
+                    {
+
+                        Console.WriteLine($"\tUploading image with tag: {tag.Name}");
+                        trainingApi.CreateImagesFromData(project.Id, image, new List<string>() { tag.Id.ToString() });
+                    }
+                    catch (Exception e)
+                    {
+                        //kill exception and carry on
+                        Console.WriteLine(e);
+
+                    }
                 }
             }
 
-            using (TextWriter writer = new StreamWriter($"{trainingSetPath}\\trainingModel.csv"))
+            try
             {
-                var csv = new CsvWriter(writer);
+                using (TextWriter writer = new StreamWriter($"{trainingSetPath}\\trainingModel.csv"))
+                {
+                    var csv = new CsvWriter(writer);
 
-                csv.WriteRecords(trainingModel);
+                    csv.WriteRecords(trainingModel);
+                }
+
+
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
         }
 
         private static Project CreateProject(string projectName)
         {
-            var trainingKey = ConfigurationManager.AppSettings["CustomVision_TrainingKey"];
-            var trainingApi = new TrainingApi() { ApiKey = trainingKey };
+            try
+            {
+                var trainingKey = ConfigurationManager.AppSettings["CustomVision_TrainingKey"];
+                var trainingApi = new TrainingApi() { ApiKey = trainingKey };
 
-            // Create a new project
-            Console.WriteLine("\tCreating new project:");
-            var project = trainingApi.CreateProject(projectName);
-            return project;
+                // Create a new project
+                Console.WriteLine("\tCreating new project:");
+                var project = trainingApi.CreateProject(projectName);
+                return project;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
 
