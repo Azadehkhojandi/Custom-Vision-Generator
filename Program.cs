60bcb4505fcb6302206ced6278045f075bf41080
Nigel Parker <nzigel@users.noreply.github.com>
2018-03-01 17:20:56 +1300

    Adding Image Augmentation and PreProcessing steps

--- Program.cs
+++ Program.cs
@@ -13,6 +13,12 @@ using CustomVisionEnd2End.Services;
 using Microsoft.Cognitive.CustomVision.Prediction;
 using Microsoft.Cognitive.CustomVision.Training;
 using Microsoft.Cognitive.CustomVision.Training.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace CustomVisionEnd2End
 {
@@ -61,17 +67,17 @@ namespace CustomVisionEnd2End
             Console.WriteLine($"\tReading Tags from {tsgfilepath}");
             var tags = ReadTags(tsgfilepath);
 
-
             //generate random name for the project 
-             
-            var projectName = tags.Count == 2 ? $"{tags[0]} {tags[1]} classifier {DateTime.Now:yyyyMMddHHmm}" :  DateTime.Now.ToString("yyyyMMddHHmm");
+
+            var projectName = tags.Count == 2 ? $"{tags[0]} {tags[1]} classifier {DateTime.Now:yyyyMMddHHmm}" : DateTime.Now.ToString("yyyyMMddHHmm");
             Console.WriteLine($"\tCreating a project in customvision.ai - project name: {projectName}");
+            
             var project = CreateProject(projectName);
 
             //download images - 
             var imagesresouce = $"{basepath}\\{projectName}\\data\\";
-            var trainingSetPath = $"{basepath}\\{projectName}\\data\\TrainingSet";
-            var testSetPath = $"{basepath}\\{projectName}\\data\\TestSet";
+            var trainingSetPath = $"{imagesresouce}TrainingSet";
+            var testSetPath = $"{imagesresouce}TestSet";
             Console.WriteLine($"\tBing search & downloading images - split them in TrainingSet & TestSet for each tag");
             Console.WriteLine($"\tTrainingSetPath: {trainingSetPath}");
             Console.WriteLine($"\tTestSetPath: {testSetPath}");
@@ -79,8 +85,15 @@ namespace CustomVisionEnd2End
             System.IO.Directory.CreateDirectory(testSetPath);
 
 
-            int minTrainingPhotosCount = 5;
-            int minTestPhotosCount = 3;
+            //int minTrainingPhotosCount = 60; //60;
+            //int minTestPhotosCount = 20; // 20;
+
+            int sizeOfImageSet = 10;
+            try
+            {
+                sizeOfImageSet = Convert.ToInt16(ConfigurationManager.AppSettings["sizeOfTrainingImageSet"]); //recommend 100
+            }
+            catch { }
 
             foreach (var tag in tags)
             {
@@ -89,7 +102,7 @@ namespace CustomVisionEnd2End
                     {
                         Console.WriteLine($"\tStarting the Process for : {tag}");
 
-                        var bingresult = await bingImageSearchService.ImageSearch(tag, 20);
+                        var bingresult = await bingImageSearchService.ImageSearch(tag, sizeOfImageSet);
                         if (bingresult.value == null) return;
                         //
                         using (var writer = new StreamWriter($"{imagesresouce}\\{tag}_resource.csv"))
@@ -100,22 +113,24 @@ namespace CustomVisionEnd2End
                             }
                         }
 
-                    //
-                    //training 
-                    Console.WriteLine($"\tDownloading the training set");
-                        var trainingphotos = DownloadImages($"{trainingSetPath}\\{tag}", bingresult.value.ToList(), minTrainingPhotosCount);
+                        //
+                        //training 
+                        Console.WriteLine($"\tDownloading the training and test set");
+                        var trainingphotos = DownloadImages(projectName, tag, bingresult.value.ToList(), sizeOfImageSet);
                         //test
-                        Console.WriteLine($"\tDownloading the test set");
-                        var testphotos = DownloadImages($"{testSetPath}\\{tag}", bingresult.value.Skip(trainingphotos).ToList(), minTestPhotosCount);
-                       
-                        if (trainingphotos < minTrainingPhotosCount || testphotos < minTestPhotosCount)
-                        {
-                            throw new Exception($"Bing couldn't find required images.you need at least 2 tags and 5 images for each tag to start");
-                        }
+                        //Console.WriteLine($"\tDownloading the test set");
+                        //var testphotos = DownloadImages($"{testSetPath}\\{tag}", bingresult.value.Skip(trainingphotos).ToList(), minTestPhotosCount, false);
+
+                        //if (trainingphotos < minTrainingPhotosCount || testphotos < minTestPhotosCount)
+                        //{
+                        //    throw new Exception($"Bing couldn't find required images.you need at least 2 tags and 5 images for each tag to start");
+                        //}
                     }
                 }
 
             }
+
+            
 
             CreateTheModel(trainingSetPath, project);
 
@@ -126,7 +141,6 @@ namespace CustomVisionEnd2End
 
             TestingTheModel(testSetPath, project);
 
-
         }
 
         private static List<string> ReadTags(string tsgfilepath)
@@ -134,7 +148,7 @@ namespace CustomVisionEnd2End
             try
             {
                 var result = new List<string>();
-                
+
                 using (TextReader fileReader = File.OpenText(tsgfilepath))
                 {
                     var csv = new CsvReader(fileReader);
@@ -162,12 +176,267 @@ namespace CustomVisionEnd2End
             }
         }
 
-        private static int DownloadImages(string pathString, List<Value> items, int numberofimagestodownload = -1)
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        /// <summary>
+        /// Formats the given JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string to format.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            string INDENT_STRING = "    ";
+            var indent = 0;
+            var quoted = false;
+            var sb = new StringBuilder();
+            for (var i = 0; i < json.Length; i++)
+            {
+                var ch = json[i];
+                switch (ch)
+                {
+                    case '{':
+                    case '[':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, ++indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, --indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        sb.Append(ch);
+                        break;
+                    case '"':
+                        sb.Append(ch);
+                        bool escaped = false;
+                        var index = i;
+                        while (index > 0 && json[--index] == '\\')
+                            escaped = !escaped;
+                        if (!escaped)
+                            quoted = !quoted;
+                        break;
+                    case ',':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case ':':
+                        sb.Append(ch);
+                        if (!quoted)
+                            sb.Append(" ");
+                        break;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+
+
+        /// <summary>
+        /// Gets a thumbnail image from the specified image file by using the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to use to create the thumbnail image.</param>
+        static async void SmartResizeImage(string imageurl, string pathString, string filename, bool isTraining)
+        {
+            string imageFilePath = pathString + "\\" + filename;
+
+            HttpClient client = new HttpClient();
+
+            string subscriptionKey = ConfigurationManager.AppSettings["ComputerVision_Key"];
+            // e.g. https://westus2.api.cognitive.microsoft.com/vision/v1.0/generateThumbnail
+            string uriBase = ConfigurationManager.AppSettings["ComputerVision_Url"];
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters.
+            // setting the size for the image is dependant on the model for transfer learning and you can experiment with this to try and get the best results for your dataset
+            // '299', '224', '192', '160', or '128' for the input image size, with smaller sizes giving faster speeds during training
+            // I recommend if you plan to export the model generated for tensorflow transfer learning with ResNet V2 models use image size of 299
+            string requestParameters = "width=299&height=299&smartCropping=true";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            //byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            var jsonBody = "{'url': '" + imageurl + "'}";
+            byte[] byteData = Encoding.UTF8.GetBytes(jsonBody);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                //content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsByteArrayAsync();
+                    System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(new MemoryStream(responseContent));
+                    AugmentImage(bitmap, pathString, filename, isTraining);
+
+                    /*
+                    // write the image file
+                    var responseContent = await response.Content.ReadAsByteArrayAsync();
+                    using (BinaryWriter binaryWrite = new BinaryWriter(new FileStream(imageFilePath, FileMode.Create, FileAccess.Write)))
+                    {
+                        binaryWrite.Write(responseContent);
+                    }
+                    */
+
+                }
+                else
+                {
+                    // Display the JSON error data.
+                    Console.WriteLine("\nError: Thumbnail not created\n");
+                    Console.WriteLine(JsonPrettyPrint(await response.Content.ReadAsStringAsync()));
+                }
+            }
+
+        }
+
+        /*
+        Although Transfer learning requires a smaller dataset it still needs conisitency and image preperation steps to deliver good results.
+        
+        Data preparation is required when working with neural network and deep learning models.
+        Increasingly data augmentation is also required on more complex object recognition tasks.
+
+        The more data an ML algorithm has access to, the more effective it can be. 
+        Even when the data is of lower quality, algorithms can actually perform better, as long as useful data can be extracted by the model from the original data set.
+
+        http://cs231n.stanford.edu/reports/2017/pdfs/300.pdf
+        https://arxiv.org/pdf/1609.08764.pdf
+        https://towardsdatascience.com/image-augmentation-for-deep-learning-histogram-equalization-a71387f609b2
+        http://imgaug.readthedocs.io/en/latest/source/examples_basics.html
+
+        */
+
+        private static int RandomNumberOdd(int min, int max)
+        {
+            Random random = new Random();
+            int ans = random.Next(min, max);
+            if (ans % 2 == 1) return ans;
+            else
+            {
+                if (ans + 1 <= max)
+                    return ans + 1;
+                else if (ans - 1 >= min)
+                    return ans - 1;
+                else return 0;
+            }
+        }
+
+        private static void AugmentImage(System.Drawing.Bitmap img, string pathString, string filename, bool isTraining)
+        {
+            string imageFilePath = pathString + "\\" + filename;
+            // flip from RGB to BGR
+            Image<Bgr, byte> ogImg = new Image<Bgr, byte>(img);
+
+            // perform Intensity Image Equalization
+            Image<Ycc, byte> ycrcb = ogImg.Convert<Ycc, byte>();
+            ycrcb._EqualizeHist();
+            ogImg = ycrcb.Convert<Bgr, byte>(); //replace original image with equalized image
+
+            if (isTraining) { 
+                // for training images perform additional image augmentation steps
+
+                //Small gaussian blur for about half of the training images with a random odd kernelSize 1,3 or 5
+                if (Convert.ToBoolean(new Random().Next(0, 2)))
+                {
+                    ogImg._SmoothGaussian(RandomNumberOdd(1, 5));
+                }
+
+                // - rotate by -45 to +45 degrees five times
+                for (var i = 0; i < 5; i++)
+                {
+                    WriteImageFile(ogImg.Rotate(new Random().Next(-45, 45), new Bgr(0, 0, 0)), imageFilePath, "_rotated"+i);
+                }
+
+                // flip the image horizontally
+                WriteImageFile(ogImg.Flip(Emgu.CV.CvEnum.FlipType.Horizontal), imageFilePath, "_flipped");
+
+            }
+            WriteImageFile(ogImg, imageFilePath);
+
+        }
+
+
+        private static void WriteImageFile(Image<Bgr, byte> img, string imageFilePath, string typeStr="")
+        {
+            using (BinaryWriter binaryWrite = new BinaryWriter(new FileStream(imageFilePath.Replace(".jpg", "") + typeStr+".jpg", FileMode.Create, FileAccess.Write)))
+            {
+                binaryWrite.Write(img.ToJpegData(95));
+            }
+        }
+
+
+        private static int DownloadImages(string projectName, string tag, List<Value> items, int numberofimagestodownload)
         {
             var counter = 1;
-            Directory.CreateDirectory(pathString);
+
+            var basepath = Directory.GetCurrentDirectory();
+            var imagesresouce = $"{basepath}\\{projectName}\\data\\";
+            var trainingSetPath = $"{imagesresouce}TrainingSet\\{tag}";
+            var testSetPath = $"{imagesresouce}TestSet\\{tag}";
+
+            Directory.CreateDirectory(trainingSetPath);
+            Directory.CreateDirectory(testSetPath);
+
+            string pathString;
+            bool isTraining;
+            
             foreach (var item in items)
             {
+                if ((new Random().Next(1, 11)) <= 7)
+                {
+                    // randomly 70% of the time is training 30% of the time is test
+                    isTraining = true;
+                    pathString = trainingSetPath;
+                }
+                else
+                {
+                    isTraining = false;
+                    pathString = testSetPath;
+                }
+                
+                if (counter%5==0) { 
+                    //API is rate limited wait 5 seconds after every 5 calls
+                    System.Threading.Thread.Sleep(2000);
+                }
+
                 if (numberofimagestodownload > 0 && counter > numberofimagestodownload)
                 {
                     return numberofimagestodownload;
@@ -180,8 +449,12 @@ namespace CustomVisionEnd2End
                     var webClient = new WebClient();
                     var uri = new Uri(imageurl);
                     var filename = uri.Segments.Last();
-                    webClient.DownloadFile(imageurl, pathString + "\\" + filename);
-                   
+
+                    //webClient.DownloadFile(imageurl, pathString + "\\" + filename);
+
+                    // rezise the image using smart crop service
+                    SmartResizeImage(imageurl, pathString, filename, isTraining);
+                    
                 }
                 catch (Exception e)
                 {
@@ -195,6 +468,7 @@ namespace CustomVisionEnd2End
 
         private static void TestingTheModel(string testingSetPath, Project project)
         {
+
 
             var predictionKey = ConfigurationManager.AppSettings["CustomVision_PredictionKey"];
             var predictionEndpoint = new PredictionEndpoint() { ApiKey = predictionKey };
@@ -410,7 +684,7 @@ namespace CustomVisionEnd2End
             catch (Exception e)
             {
                 Console.WriteLine(e);
-               
+
             }
 
 
@@ -470,7 +744,7 @@ namespace CustomVisionEnd2End
             catch (Exception e)
             {
                 Console.WriteLine(e);
-               
+
             }
 
         }
@@ -495,5 +769,16 @@ namespace CustomVisionEnd2End
         }
 
 
+    }
+
+    static class Extensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+        {
+            foreach (var i in ie)
+            {
+                action(i);
+            }
+        }
     }
 }
